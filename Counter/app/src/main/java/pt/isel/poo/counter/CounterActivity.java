477e6793c6b1c3e66f79595f0838c8d8e40958de
palex <palex@cc.isel.ipl.pt>
2015-05-11 21:06:20 +0100

    Versão com AlertDialog para nome do ficheiro (aula 11/05/2015)

--- Counter/app/src/main/java/pt/isel/poo/counter/CounterActivity.java
+++ Counter/app/src/main/java/pt/isel/poo/counter/CounterActivity.java
@@ -1,11 +1,15 @@
 package pt.isel.poo.counter;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,14 +24,16 @@ import java.util.Scanner;
 public class CounterActivity extends Activity {
 
     private Button inc, dec;
+    private Button save, load;
     private TextView ctr;
     private BallView ball;
-
     private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LinearLayout buttons = new LinearLayout(this);
+        buttons.setOrientation(LinearLayout.HORIZONTAL);
         LinearLayout root = new LinearLayout(this);
         root.setOrientation(LinearLayout.VERTICAL);
 
@@ -42,6 +48,20 @@ public class CounterActivity extends Activity {
         ctr.setBackgroundColor(Color.YELLOW);
         ball = new BallView(this);
 
+        save = new TxtButton("Save",buttons,new View.OnClickListener() {
+            public void onClick(View v) {
+                    showDialog(true);
+            }
+        });
+        load = new TxtButton("Load",buttons,new View.OnClickListener() {
+            public void onClick(View v) {
+                    showDialog(false);
+            }
+        });
+        //BUG: Aqui só era criado 1 objecto EditText para todas as dialog.
+        //fileName = new EditText(this);
+
+        root.addView(buttons);
         root.addView(inc);
         root.addView(ctr);
         root.addView(dec);
@@ -76,19 +96,22 @@ public class CounterActivity extends Activity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        loadFile();
+        counter = savedInstanceState.getInt("counter");
+        updateView();
+        //loadFile();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        saveFile();
+        outState.putInt("counter",counter);
+        //saveFile();
     }
 
 
-    private void saveFile() {
+    private void saveFile(String fileName) {
         try {
-            OutputStream out = openFileOutput("counter.txt",MODE_PRIVATE);
+            OutputStream out = openFileOutput(fileName,MODE_PRIVATE);
             PrintWriter bout = new PrintWriter(out);
             bout.println(counter);
             bout.close();
@@ -96,9 +119,9 @@ public class CounterActivity extends Activity {
             e.printStackTrace();
         }
     }
-    private void loadFile() {
+    private void loadFile(String fileName) {
         try {
-            InputStream in = openFileInput("counter.txt");
+            InputStream in = openFileInput(fileName);
             Scanner tin = new Scanner(in);
             counter = tin.nextInt();
             updateView();
@@ -108,4 +131,33 @@ public class CounterActivity extends Activity {
         }
     }
 
+    private class TxtButton extends Button {
+        public TxtButton(String txt, ViewGroup root, OnClickListener listener) {
+            super(getApplicationContext());
+            setText(txt);
+            setOnClickListener(listener);
+            root.addView(this);
+        }
+    }
+
+    private String lastFileName = "counter.txt";
+    private EditText fileName;
+
+    private void showDialog(final boolean save) {
+        String txt = save ? "Save" : "Load";
+        fileName = new EditText(this);  // Cada dialog com o seu EditText
+        fileName.setText(lastFileName);
+        new AlertDialog.Builder(this)
+          .setMessage("File name to "+txt)
+          .setPositiveButton(txt, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface d, int which) {
+                lastFileName = fileName.getText().toString();
+                if (save) saveFile(lastFileName);
+                else loadFile(lastFileName);
+            }
+          })
+          .setView(fileName)
+          .setNegativeButton("Cancel",null)
+          .show();
+    }
 }
